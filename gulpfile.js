4dcf1124c01b35c251fa75e77dda2514b6cc3c4c
EMERSON DOS SANTOS <emersoncavalcantes@hotmail.com.br>
2026-07-08 22:48:26 -0300

    Fix: removendo gulp-imagemin problemático e gerando dist completa

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,29 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 
-// Tarefa de compilação e minificação do SASS
+// Função que copia o HTML de 'src' para 'dist'
+function tarefasHtml() {
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./dist'));
+}
+
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Evita travar o watch em caso de erro de sintaxe
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-// Tarefa de minificação de JavaScript (scripts do site e abas)
+// Nova função de imagens: apenas copia, sem dar erro de 'imagemin'
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(gulp.dest('./dist/images')); // Apenas move os arquivos salvando o build
+}
+
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
 }
 
-// Tarefa de otimização de imagens (Layout Bazinga+)
-async function images() {
-    // Importação dinâmica necessária para versões modernas do gulp-imagemin
-    const imagemin = (await import('gulp-imagemin')).default;
-    
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
-}
-
-// Exportação das tarefas principais
-exports.default = gulp.parallel(styles, images, scripts);
-
-// Monitorador de arquivos em tempo real (Watch)
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-    gulp.watch('./src/images/**/*', gulp.parallel(images)); // Adicionado para monitorar novas imagens da série
-}
+// Executa todas as tarefas juntas de forma segura
+exports.default = gulp.parallel(styles, images, scripts, tarefasHtml);
