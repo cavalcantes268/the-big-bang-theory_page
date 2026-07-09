03517a49fea481c663747e45714c8c90ec5e03ed
EMERSON DOS SANTOS <emersoncavalcantes@hotmail.com.br>
2026-07-08 23:10:07 -0300

    Fix: estrutura do gulpfile totalmente corrigida

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,31 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 
-// Função que copia o HTML de 'src' para 'dist'
+// 1. Copia o HTML da Raiz para a pasta Dist
 function tarefasHtml() {
-    return gulp.src('./src/index.html')
+    return gulp.src('./index.html')
         .pipe(gulp.dest('./dist'));
 }
 
+// 2. Compila e comprime o CSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-// Nova função de imagens: apenas copia, sem dar erro de 'imagemin'
+// 3. Copia as imagens sem compressão para evitar erros de versão
 function images() {
     return gulp.src('./src/images/**/*')
-        .pipe(gulp.dest('./dist/images')); // Apenas move os arquivos salvando o build
+        .pipe(gulp.dest('./dist/images'));
 }
 
+// 4. Comprime os scripts JavaScript
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
 }
 
-// Executa todas as tarefas juntas de forma segura
+// Executa todas as 4 tarefas em paralelo quando você roda o build
 exports.default = gulp.parallel(styles, images, scripts, tarefasHtml);
